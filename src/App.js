dbbc65c7502a5e343572e90926b79760971f4342
datnt86 <datnt@twendeesoft.com>
2023-05-21 11:08:18 +0700

    test deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello React
+          Hello React, check deployment by Github action
         </a>
       </header>
     </div>
